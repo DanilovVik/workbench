2ff91e820dc1d27125b9cb0cbb5680d8d72ab40a
cabrucas <60257049+cabrucas@users.noreply.github.com>
2020-02-29 13:46:22 +0300

    Add files via upload

---
+++ GetFormData.js
@@ -0,0 +1,19 @@
+function GetFormData() {
+    let forms = document.getElementsByClassName('btn');
+    for (let form of forms) {
+        form.onclick = function() {
+            let formID = form.parentNode.getAttribute("id");
+            let formChooser = document.getElementById(formID);
+            let inputs = formChooser.querySelectorAll('input');
+            let dataOutput = new Array();
+            dataOutput.push(formID);
+            for (let input of inputs) {
+                var attributeName = input.getAttribute("name");
+                var fieldValue = input.value;
+                dataOutput.push(attributeName + ": " + fieldValue);
+            }
+            console.log(dataOutput);
+        }
+    }
+}
+GetFormData();
